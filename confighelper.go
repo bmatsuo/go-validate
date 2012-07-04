1e4867b852d1416877038aab0eed5ebb3e6defb5
Bryan Matsuo <bryan.matsuo@gmail.com>
2012-07-03 23:27:22 -0700

    Empty project generated by gonew.

---
+++ confighelper.go
@@ -0,0 +1,16 @@
+// Copyright 2012, Bryan Matsuo. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+/*  Filename:    confighelper.go
+ *  Author:      Bryan Matsuo <bryan.matsuo [at] gmail.com>
+ *  Created:     2012-07-03 23:27:22.212922 -0700 PDT
+ *  Description: Main source file in confighelper
+ */
+
+
+// Package confighelper does ....
+package confighelper
+
+import (
+)
---
+++ confighelper_test.go
@@ -0,0 +1,21 @@
+// Copyright 2012, Bryan Matsuo. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package confighelper
+
+/*  Filename:    confighelper_test.go
+ *  Author:      Bryan Matsuo <bryan.matsuo [at] gmail.com>
+ *  Created:     2012-07-03 23:27:22.213151 -0700 PDT
+ *  Description: Main test file for confighelper
+ */
+
+import (
+    "testing"
+)
+
+
+func TestConfighelper(t *testing.T) {
+
+}
+
